6402e473e58cd841b99a364e17d24925158436fe
abrahamve <abrahamve_01@hotmail.com>
2024-11-15 10:20:26 -0600

    meter vite2

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/js/app.js', //hola
-                'resources/css/app.css'
+                'resources/js/app.js',
+                'resources/css/app.css', //agg vite 2
             ],
             refresh: true,
         }),
