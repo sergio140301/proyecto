87d3f32a51467a00078a2caa3ab371fa564089c7
abrahamve <abrahamve_01@hotmail.com>
2024-11-15 09:04:59 -0600

    meter vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js', //hola
+                'resources/css/app.css'
             ],
             refresh: true,
         }),
