8dc9b57ee4b1baa4f2c098e1d9324e398190873b
abrahamve <abrahamve_01@hotmail.com>
2024-12-03 01:28:16 -0600

    agg materis al alumno abiertas de su horario

--- database/migrations/2024_12_03_071353_create_horario_alumnos_table.php
+++ database/migrations/2024_12_03_071353_create_horario_alumnos_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,6 +18,17 @@ return new class extends Migration
             $table->unsignedBigInteger('grupo_id');  // Campo grupo_id bigint UN
             $table->timestamps(); // Campos created_at y updated_at como timestamps
         });
+
+            // Insertar valores fijos
+            DB::table('horario_alumnos')->insert([
+                ['id' => 22, 'materia_id' => 1, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:12:44', 'updated_at' => '2024-12-03 05:12:44'],
+                ['id' => 23, 'materia_id' => 1, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:07', 'updated_at' => '2024-12-03 05:16:07'],
+                ['id' => 24, 'materia_id' => 2, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:07', 'updated_at' => '2024-12-03 05:16:07'],
+                ['id' => 25, 'materia_id' => 3, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:07', 'updated_at' => '2024-12-03 05:16:07'],
+                ['id' => 26, 'materia_id' => 1, 'periodo_tutoria_id' => 26,  'alumno_id' => 1,'created_at' => '2024-12-03 05:16:10', 'updated_at' => '2024-12-03 05:16:10'],
+                ['id' => 27, 'materia_id' => 2, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:10', 'updated_at' => '2024-12-03 05:16:10'],
+                ['id' => 28, 'materia_id' => 3, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:10', 'updated_at' => '2024-12-03 05:16:10'],
+            ]);
     }
 
     /**
