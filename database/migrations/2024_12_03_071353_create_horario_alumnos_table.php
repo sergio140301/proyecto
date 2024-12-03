f8271b7922970bb96081eda07a07cbd3f63bd9c1
abrahamve <abrahamve_01@hotmail.com>
2024-12-03 01:31:17 -0600

    agg materis al alumno abiertas de su horario2

--- database/migrations/2024_12_03_071353_create_horario_alumnos_table.php
+++ database/migrations/2024_12_03_071353_create_horario_alumnos_table.php
@@ -21,13 +21,10 @@ return new class extends Migration
 
             // Insertar valores fijos
             DB::table('horario_alumnos')->insert([
-                ['id' => 22, 'materia_id' => 1, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:12:44', 'updated_at' => '2024-12-03 05:12:44'],
-                ['id' => 23, 'materia_id' => 1, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:07', 'updated_at' => '2024-12-03 05:16:07'],
-                ['id' => 24, 'materia_id' => 2, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:07', 'updated_at' => '2024-12-03 05:16:07'],
-                ['id' => 25, 'materia_id' => 3, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:07', 'updated_at' => '2024-12-03 05:16:07'],
-                ['id' => 26, 'materia_id' => 1, 'periodo_tutoria_id' => 26,  'alumno_id' => 1,'created_at' => '2024-12-03 05:16:10', 'updated_at' => '2024-12-03 05:16:10'],
-                ['id' => 27, 'materia_id' => 2, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:10', 'updated_at' => '2024-12-03 05:16:10'],
-                ['id' => 28, 'materia_id' => 3, 'periodo_tutoria_id' => 26, 'alumno_id' => 1, 'created_at' => '2024-12-03 05:16:10', 'updated_at' => '2024-12-03 05:16:10'],
+                ['id' => 22, 'alumno_id' => 1, 'grupo_id'=> 10, 'created_at' => '2024-12-03 05:12:44', 'updated_at' => '2024-12-03 05:12:44'],
+                ['id' => 23, 'alumno_id' => 1, 'grupo_id'=> 11, 'created_at' => '2024-12-03 05:16:07', 'updated_at' => '2024-12-03 05:16:07'],
+                ['id' => 24, 'alumno_id' => 1, 'grupo_id'=> 12, 'created_at' => '2024-12-03 05:16:07', 'updated_at' => '2024-12-03 05:16:07'],
+ 
             ]);
     }
 
