3e55092b62a4788556d1a6c26bce26c254dae6c6
abrahamve <abrahamve_01@hotmail.com>
2024-12-03 00:52:51 -0600

    otro cambio

--- app/Models/periodoTutoria.php
+++ app/Models/periodoTutoria.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class periodoTutoria extends Model
+class PeriodoTutoria extends Model
 {
     /** @use HasFactory<\Database\Factories\PeriodoTutoriaFactory> */
     use HasFactory;
